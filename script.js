0354d68526b44f485b3d4c2e4f4049135926e09a
michaelleahy44 <mjleahy3@gmail.com>
2024-09-27 20:37:41 -0400

    Updated project files

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
-/* Set the width of the sidebar to 100% (show it) */
+/* Set the width of the sidebar to 250px (show it) */
 function openNav() {
   document.getElementById("mySidepanel").style.width = "100%";
+  document.getElementById("mySidepanel").style.height = "100%";
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
   document.getElementById("mySidepanel").style.width = "0";
+  document.getElementById("mySidepanel").style.height = "0";
 }
